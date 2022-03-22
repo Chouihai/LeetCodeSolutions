bfa7c3a1183c26813043fbf6adb3ecf5c84f0cb3
Haitam <chouiekh@uwm.edu>
2022-03-21 22:34:40 -0500

    Changed name

--- src/com/company/PlusOne.java
+++ src/com/company/PlusOne.java
@@ -1,8 +1,25 @@
 package com.company;
 
-public class PlusOne {
+import java.util.Arrays;
 
+public class PlusOne {
     public static void main(String[] args) {
-	// write your code here
+        int[] bruh = {1,2,3,4,5};
+        System.out.println(Arrays.toString(plusOne(bruh)));
+    }
+
+    public static int[] plusOne(int[] digits) {
+
+        for(int i=digits.length-1; i>=0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newArray = new int[digits.length+1];
+        newArray[0] = 1;
+        return newArray;
     }
 }
