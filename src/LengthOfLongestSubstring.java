0ed50c2f4804c421a31e57986e21b90e5ad6fe13
Haitam <chouiekh@uwm.edu>
2022-03-22 17:46:02 -0500

    Finished naive approach of number 3

---
+++ src/LengthOfLongestSubstring.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("au"));
+    }
+    public static int lengthOfLongestSubstring(String s) {
+       ArrayList<Character> list = new ArrayList<>();
+       int lls = 0;
+       for (int i =0 ; i<s.length();i++){
+           list.add(s.charAt(i));
+       }
+       if (list.size() ==1) return 1;
+        while (list.size()>1){
+             ArrayList<Character> SubList = new ArrayList<>();
+             SubList.add(list.get(0));
+             int k = 1;
+             boolean Repeat = false;
+             while(!Repeat) {
+                 for (char c : SubList) {
+                     if (c == list.get(k)) {
+                         Repeat = true;
+                     }
+                 }
+                 if (!Repeat) {
+                     SubList.add(list.get(k));
+                     k++;
+                     if (k == list.size()) Repeat = true;
+                 }
+             }
+             if (k >= lls) lls =k;
+             list.remove(0);
+        }
+        return lls;
+    }
+}
